b0e42b82c3a1f13bc7056d76d17cd6f7aab6f72a
Ederson <edersonrp@gmail.com>
2022-08-16 17:21:35 -0300

    add vinculados

---
+++ vinculados.sql
@@ -0,0 +1,76 @@
+Declare @pedido INT
+set @pedido = 6548912 --Medley
+
+
+
+-- Orçamento
+SELECT a.IdMovimento,
+       a.NúmeroDocumento,
+       a.Descrição,
+       a.IdCadastro,
+       a.IdUsuário,
+       a.TotalDocumento,
+       a.DataMovimento,
+       a.IdCancelamento,
+       a.IdUsuarioCancelamento,
+       a.MotivoCancelamento,
+       a.LiberaçãoUsuário
+FROM Ourobase.dbo.Mov_Estoque a
+WHERE a.IdMovimento in
+      (SELECT DISTINCT fk_int_Movimento
+       FROM OuroWebServiceLive.dbo.tab_Pedido
+       WHERE fk_int_PedidoSite = @pedido)
+   or a.IdMovimento in (SELECT DISTINCT int_IdMovimento
+                        FROM Ourobase.dbo.Tab_FarmaPedido
+                        WHERE int_NroPedidoSite = @pedido)
+
+UNION ALL
+
+-- Pedido
+SELECT a.IdMovimento,
+       a.NúmeroDocumento,
+       a.Descrição,
+       a.IdCadastro,
+       a.IdUsuário,
+       a.TotalDocumento,
+       a.DataEmissão,
+       a.IdCancelamento,
+       a.IdUsuarioCancelamento,
+       a.MotivoCancelamento,
+       a.LiberaçãoUsuário
+FROM Ourobase.dbo.Mov_Estoque a
+WHERE a.IdOrçamentoUtilizado =
+      (SELECT DISTINCT fk_int_Movimento
+       FROM OuroWebServiceLive.dbo.tab_Pedido
+       WHERE fk_int_PedidoSite = @pedido)
+   or a.IdOrçamentoUtilizado = (SELECT DISTINCT int_IdMovimento
+                                FROM Ourobase.dbo.Tab_FarmaPedido
+                                WHERE int_NroPedidoSite = @pedido)
+
+
+UNION ALL
+-- Venda
+SELECT a.IdMovimento,
+       a.NúmeroDocumento,
+       a.Descrição,
+       a.IdCadastro,
+       a.IdUsuário,
+       a.TotalDocumento,
+       a.DataMovimento,
+       a.IdCancelamento,
+       a.IdUsuarioCancelamento,
+       a.MotivoCancelamento,
+       a.LiberaçãoUsuário
+FROM Ourobase..Mov_Estoque a
+WHERE a.IdPedidoUtilizado in
+      (SELECT DISTINCT a.IdMovimento
+       FROM Ourobase..Mov_Estoque a
+       WHERE a.IdOrçamentoUtilizado =
+             (SELECT DISTINCT fk_int_Movimento
+              FROM OuroWebServiceLive.dbo.tab_Pedido
+              WHERE fk_int_PedidoSite = @pedido))
+   or a.IdPedidoUtilizado = (SELECT DISTINCT IdMovimento
+                             FROM Ourobase.dbo.Mov_estoque
+                             Where IdOrçamentoUtilizado = (SELECT DISTINCT int_IdMovimento
+                                                           FROM Ourobase.dbo.Tab_FarmaPedido
+                                                           WHERE int_NroPedidoSite = @pedido))
